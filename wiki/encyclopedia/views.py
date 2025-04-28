b3023c76965ddfe995bde4f295d4e43f9d73876a
bohrium2b <dzm2026@gmail.com>
2025-04-28 23:56:30 +0000

    Create distro code

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def wiki(request, title):
+    return HttpResponse(f"You're looking at the wiki page for {title}.")
+
+def index(request):
+    return HttpResponse("Not Yet Implemented. <br/><img src='https://http.cat/501' alt='Too Early HTTP Status Cat'>", status=501)
