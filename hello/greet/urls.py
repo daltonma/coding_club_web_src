39102ad2d6edadcc05f579e0fe061e2df8b3ce9f
bohrium2b <dzm2026@gmail.com>
2025-04-28 23:28:21 +0000

    Add templated example

--- hello/greet/urls.py
+++ hello/greet/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('name/<str:name>', views.templated_name, name="name")
 ]
--- hello/greet/views.py
+++ hello/greet/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World!")
+
+
+def name(request, name):
+    return HttpResponse("Hello, " + name + "!")
+
+
+def templated_name(request, name):
+    return render(request, "greet/name.html", {"name": name})
