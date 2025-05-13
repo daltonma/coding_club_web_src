313173f26496ed7dc5b9fe7b576772f4d255f5ce
bohrium2b <dzm2026@gmail.com>
2025-05-13 22:54:22 +0000

    Update data for more groups, more generations

--- kpop_use.py
+++ kpop_use.py
@@ -4,10 +4,14 @@ from peewee import SqliteDatabase
 db = SqliteDatabase('kpop.db')
 # Example data
 # Create a group
+db.begin()
 group1 = Group.create(name="ENHYPEN", debut_date="2020-11-30", company="BELIFT LAB", generation=4)
 group2 = Group.create(name="TXT", debut_date="2019-03-04", company="Big Hit Entertainment", generation=4)
 group3 = Group.create(name="SEVENTEEN", debut_date="2015-05-26", company="Pledis Entertainment", generation=3)
 group4 = Group.create(name="Stray Kids", debut_date="2018-03-25", company="JYP Entertainment", generation=4)
+group5 = Group.create(name="ZEROBASEONE", debut_date="2023-07-10", company="Wake One Entertainment", generation=5)
+group6 = Group.create(name="NewJeans", debut_date="2022-08-01", company="ADOR", generation=4)
+
 
 jungwon = Artist.create(name="Yang Jungwon", stage_name="Jungwon", group=group1)
 sunghoon = Artist.create(name="Park Sung-hoon", stage_name="Sunghoon", group=group1)
@@ -22,6 +26,14 @@ woozi = Artist.create(name="Lee Jihoon", stage_name="Woozi", group=group3)
 felix = Artist.create(name="Lee Yong-bok", stage_name="Felix", group=group4)
 bangchan = Artist.create(name="Bang Chan", stage_name="Bang Chan", group=group4)
 leeknow = Artist.create(name="Lee Min-ho", stage_name="Lee Know", group=group4)
+zhanghao = Artist.create(name="Zhang Hao", stage_name="Zhang Hao", group=group5)
+yujin = Artist.create(name="Han Yujin", stage_name="Yujin", group=group5)
+hanbin = Artist.create(name="Sung Han-bin", stage_name="Hanbin", group=group5)
+danielle = Artist.create(name="Danielle Marsh", stage_name="Danielle", group=group6)
+haerin = Artist.create(name="Kim Haerin", stage_name="Haerin", group=group6)
+# Commit the changes
+db.commit()
+
 # Querying the database
 # Get all groups
 groups = Group.select()
