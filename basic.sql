623beaf9f37d53f4bddf5141be5acbe05169cc9b
Dalton Ma <85321730+bohrium2b@users.noreply.github.com>
2025-05-14 02:53:29 +0000

    Get rid of jeonghan and add generation order by

--- basic.sql
+++ basic.sql
@@ -1,4 +1,4 @@
 SELECT * FROM artist WHERE group_id = 1;
 SELECT * FROM artist WHERE group_id = 3;
-INSERT INTO artist VALUES (14, 'Yoon Jeong-han', 'Jeonghan', 3);
-SELECT * FROM artist WHERE group_id = 3;
+-- INSERT INTO artist VALUES (14, 'Yoon Jeong-han', 'Jeonghan', 3);
+SELECT * FROM `group` ORDER BY generation;
