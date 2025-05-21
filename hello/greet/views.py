f05103d0b1ad6adec6d716ef0d38c53e9976333d
bohrium2b <dzm2026@gmail.com>
2025-05-21 03:56:04 +0000

    Add christmas demo

--- hello/greet/views.py
+++ hello/greet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from datetime import date
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,19 @@ def name(request, name):
 
 def templated_name(request, name):
     return render(request, "greet/name.html", {"name": name})
+
+
+def christmas(request):
+    date_today = date.today()
+    todayischristmas = date_today.month == 12 and date_today.day == 25
+    return render(
+        request,
+        "greet/christmas.html",
+        {
+            "today": date_today.today(),
+            "todayischristmas": todayischristmas,
+            "days_until_christmas": 0
+            if todayischristmas
+            else (date(2023, 12, 25) - date_today).days,
+        },
+    )
